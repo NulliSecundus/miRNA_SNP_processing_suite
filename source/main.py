a8c05d5d23f8cc11053f582adea73c32cc100f6e
bdrackley <brendandrackley@gmail.com>
2017-08-28 13:39:53 -0500

    recommit

--- source/main.py
+++ source/main.py
@@ -1,62 +1,74 @@
 '''
 Created on Aug 22, 2017
 
-@author: JayYang
+@author: Brendan Drackley
 '''
+
+import tkinter as tk
+from tkinter import filedialog
 
 if __name__ == '__main__':
     pass
 
-container = [0]
+root = tk.Tk()
+root.withdraw()
+chromReport = filedialog.askopenfilename()
+snpFasta = filedialog.askopenfilename()
+
+refInfo = []
+with open(chromReport) as f:
+    for line in f:
+        lineSplit = line.split("\t")
+        refInfo.append(lineSplit[:2])
+refInfo = refInfo[7:]
 sequence = ""
 header = ""
+gate = False
+index = 0
 
 # file input name - change as needed
-with open("rs_chY.fas") as f:
+with open(snpFasta) as f:
     for line in f:
-        #do something
-        # print(line)
         if line[0]==">":
             sequence = ""
-            container = [0]
-            container[0] = line
+            header = line
+            header = header.replace('\n', '')
+            headerSplt = header.split("|")
+            rsNum = headerSplt[2].split(" ")
+            rsNum = rsNum[0][2:]
+            gate = (int(refInfo[index][1])==2)
+            #gate = True
+                
+            pos = headerSplt[3]
+            pos = int(pos[4:])
+                
+            alleles = headerSplt[8]
+            alleles = alleles[8:]
+            alleles = alleles.replace('\"','')
+            alleles = alleles.split('/')
+            index += 1
         elif line[0]=="\n":
-            for i in container:
-                if i[0]!=">":
-                    sequence += i
-            with open("snp_list.fasta", "a") as text_file:
-                header = container[0]
-                header = header.replace('\n', '')
-                headerSplt = header.split("|")
-                
-                pos = headerSplt[3]
-                pos = int(pos[4:])
-                
-                alleles = headerSplt[8]
-                alleles = alleles[8:]
-                alleles = alleles.replace('\"','')
-                alleles = alleles.split('/')
-                
-                sequence = sequence.replace('\n', '')
-                sequence = sequence.replace(' ', '')
-                sequence += '\n'
-                #print(sequence[pos-1])
-                
-                for allele in alleles:
-                    if pos>=26:
-                        seqChop = sequence[pos-26:pos-1]
-                    seqChop += " "
-                    if allele=='-':
-                        seqChop += ''
-                    else:
-                        seqChop += allele
+            if gate:
+                with open("snp_list_chrY.fasta", "a") as text_file:
+                    sequence = sequence.replace('\n', '')
+                    sequence = sequence.replace(' ', '')
+                    sequence += '\n'
+                    
+                    for allele in alleles:
+                        if pos>=26:
+                            seqChop = sequence[pos-26:pos-1]
                         seqChop += " "
-                    seqChop += sequence[pos:pos+25]
-                    seqChop += "\n"
-                    print("{}".format(header), file=text_file)
-                    print("{}".format(seqChop), file=text_file)
+                        if allele=='-':
+                            seqChop += ''
+                        else:
+                            seqChop += allele
+                            seqChop += " "
+                        seqChop += sequence[pos:pos+25]
+                        seqChop += "\n"
+                        print("{}".format(header), file=text_file)
+                        print("{}".format(seqChop), file=text_file)
         elif line[0]=="#":
             #Do nothing, it's a comment
-            comment = line
+            pass
         else:
-            container.append(line)
+            sequence += line
