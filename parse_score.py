f9f57ad51ab8ee01e90dc00087dc35215309febb
Brendan Drackley <bdrackley@wisc.edu>
2017-09-22 14:22:46 -0500

    refined parsescore verbose option

--- parse_score.py
+++ parse_score.py
@@ -3,20 +3,20 @@ import numpy as np
 
 @click.command()
 @click.argument('mirandafile')
-@click.option('-o', default='off', help='Print the parsed scores to the specified output file')
+@click.option('-so', default='off', help='Print the parsed scores to the specified output file')
 @click.option('--verbose', is_flag=True, help='Keep a count of the number of scores processed and print to console')
 @click.option('-ut', default=90.0, help='The upper threshold in which the top percentile is calculated')
 @click.option('-lt', default=10.0, help='The lower threshold in which the bottom percentile is calculated')
-def cli(mirandafile, o, verbose, ut, lt):
+def cli(mirandafile, so, verbose, ut, lt):
 	try:
-		parseScore(mirandafile, o, verbose, ut, lt)
+		parseScore(mirandafile, so, verbose, ut, lt)
 	except:
 		print('Error parsing scores')
 
 def parseScore(mirandaOut, outputFile, v, ut, lt):
 	scorelist = []
 	with open(mirandaOut) as f:
-		print('miranda output file opened')
+		print('processing miranda output file')
 		count = 0
 		for line in f:
 			if line[0]=='>':
@@ -24,17 +24,17 @@ def parseScore(mirandaOut, outputFile, v, ut, lt):
 				scorelist.append(float(lineSplit[2]))
 				if v:
 					count += 1
-					if count%1000==0:
+					if count%10000==0:
 						print(count)
 	if outputFile!="off":
 		try:
 			with open(outputFile, "a") as text_file:
-				print('output file opened')
+				print('Writing scores to output file')
 				for score in scorelist:
 					print('{}'.format(score), file=text_file)
 		except:
 			print('Could not print to output file')
-    up = np.percentile(scorelist, ut)  
+	up = np.percentile(scorelist, ut)  
 	print('Top ', ut, ' percentile is: ', up)
 	lp = np.percentile(scorelist, lt)  
 	print('Bottom ', lt, ' percentile is: ', lp)
