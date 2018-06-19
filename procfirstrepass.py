81fc2280ead8284ccdf7237775f13de9bc0fe34a
NulliSecundus <bdrackley@wisc.edu>
2018-06-19 14:19:06 -0500

    included procfirstrepass.py script

---
+++ procfirstrepass.py
@@ -0,0 +1,63 @@
+import click
+
+@click.command()
+@click.argument('mirandafile')
+@click.argument('procsnpfile')
+@click.argument('output')
+def cli(mirandafile, procsnpfile, output):
+    try:
+		# Run processInput on original miranda output
+        processInput(mirandafile, procsnpfile)
+    except:
+        print("Failed to parse miranda file")
+		
+	try:
+		# Run iterateMiranda on reprocess list
+		iterateMiranda()
+	except:
+		printo("Failed to re-process with miranda")
+
+if __name__ == '__main__':
+    pass
+
+reprocessList = []
+	
+def processInput(mirandaFile, procSnpFasta):
+	snpInfo = []
+	
+    try:
+        count = 0
+		header = ""
+        with open(procSnpFasta) as f:
+            for line in f:
+                if line[0]==">":
+                    header = line
+                    header = header.replace('\n', '')
+					
+					snpName = header.split(" ")
+					snpName = snpName[0][1:]
+                    
+                    headerSplt = header.split("|")
+					alleles = headerSplt[8]
+                    alleles = alleles[8:]
+                    alleles = alleles.replace('\"','')
+                    alleles = alleles.split('/')
+					alleleNum = len(alleles)
+					
+					snpInfo.append([[snpName][alleleNum]])
+					
+					if count == 10:
+						print(snpInfo)
+						return
+					
+					count = count+1
+                else:
+                    pass
+    except:
+        print('Could not parse processed snp fasta file')
+
+def iterateMiranda():
+    try: 
+		print("success")
+	except:
+		print("error")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
 	name='miRanda_input_processing',
 	version='1.0',
-	py_modules=['process_chromosome', 'parse_score', 'parse_mir', 'compressmir'],
+	py_modules=['process_chromosome', 'parse_score', 'parse_mir', 'compressmir', 'procfirstrepass'],
 	install_requires=[
 		'Click',
 		'numpy'
@@ -15,5 +15,6 @@ setup(
 		parsescore=parse_score:cli
 		parsemir=parse_mir:cli
 		compressmir=compressmir:cli
+		procfirstrepass=procfirstrepass:cli
 	''',
 )
