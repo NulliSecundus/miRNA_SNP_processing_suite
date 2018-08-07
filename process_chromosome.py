13398e76217ed42534510766d64ffbe56a048f85
NulliSecundus <bdrackley@wisc.edu>
2018-08-07 15:33:27 -0500

    updated process_chromosome.py

--- process_chromosome.py
+++ process_chromosome.py
@@ -12,111 +12,103 @@ index = 0
 def cli(reportfile, snpfile, output, verbose):
 	try:
 		processInput(reportfile, snpfile, output, verbose)
+		print("Success")
 	except:
-		print('Error processing the chromosome fasta file.')
-
-if __name__ == '__main__':
-	pass
+		print("Error")
 
 def processInput(chromReport, snpFasta, outputFile, v):
 	global refInfo
-	try:
-		with open(chromReport) as f:
-			for x in range(7):
-				next(f)
-			for line in f:
-				lineSplit = line.split("\t")
-				lineSplit = lineSplit[:13]
-				tempLine = [
-					int(lineSplit[0]),
-					int(lineSplit[1]),
-					lineSplit[12]
-				]
-				refInfo.append(tempLine)
-		sequence = ""
-		header = ""
-		unique = False
-		gene = False
-		stdSNP = False
-	except:
-		print('Could not read chromosome report file')
+	with open(chromReport) as f:
+		for x in range(7):
+			next(f)
+		for line in f:
+			lineSplit = line.split("\t")
+			lineSplit = lineSplit[:13]
+			tempLine = [
+				int(lineSplit[0]),
+				int(lineSplit[1]),
+				lineSplit[12]
+			]
+			refInfo.append(tempLine)
+	sequence = ""
+	header = ""
+	unique = False
+	gene = False
+	stdSNP = False
     
-	try:
-		count = 0
-		with open(snpFasta) as f:
-			with open(outputFile, "a") as text_file:
-				for line in f:
-					if line[0]==">":
-						sequence = ""
-						header = line
-						header = header.replace('\n', '')
-						headerSplt = header.split("|")
-						rs = headerSplt[2].split(" ")
-						rsNum = int(rs[0][2:])
+	count = 0
+	with open(snpFasta) as f:
+		with open(outputFile, "a") as text_file:
+			for line in f:
+				if line[0]==">":
+					sequence = ""
+					header = line
+					header = header.replace('\n', '')
+					headerSplt = header.split("|")
+					rs = headerSplt[2].split(" ")
+					rsNum = int(rs[0][2:])
 
-						tempLine = rsSearch(rsNum)
-						if tempLine == None :
-							unique = False
-							gene = False
-							print("Error")
-							return
-						else:
-							unique = (tempLine[1]==2)
-							gene = (len(tempLine[2])>1)
-
-						pos = headerSplt[3]
-						pos = int(pos[4:])
-
-						snpClass = headerSplt[7]
-						snpClass = snpClass[6:]
-						stdSNP = (snpClass == '1')
-							
-						alleles = headerSplt[8]
-						alleles = alleles[8:]
-						alleles = alleles.replace('\"','')
-						alleles = alleles.split('/')
-						alleleNum = len(alleles)
-						
-					elif line[0]=="\n":
-						if unique and gene and stdSNP and (pos>25):
-							sequence = sequence.replace('\n', '')
-							sequence = sequence.replace(' ', '')
-							sequence += '\n'
-							
-							if len(sequence) < 50:
-								continue
-								
-							for allele in alleles:
-								seqChop = sequence[pos-26:pos-1]
-								if allele=='-':
-									seqChop += ''
-								else:
-									seqChop += allele
-								seqChop += sequence[pos:pos+25]
-								seqChop += "\n" 
-								
-								# Modify header with allele info
-								label = header.split(" ")
-								label = label[0]
-								label += "|" + str(alleleNum)
-								label += "|" + allele
-								
-								# Print each header/sequence to file 
-								print("{}".format(label), file=text_file)
-								print("{}".format(seqChop), file=text_file)
-								
-								# Increment counter
-								count += 1
-								if v:
-									if count%10000==0:
-										print(count)
-					elif line[0]=="#":
-						#Do nothing, it's a comment line
-						pass
+					tempLine = rsSearch(rsNum)
+					if tempLine == None :
+						unique = False
+						gene = False
+						print("Error")
+						return
 					else:
-						sequence += line
-	except:
-		print('Could not read snp fasta file')
+						unique = (tempLine[1]==2)
+						gene = (len(tempLine[2])>1)
+
+					pos = headerSplt[3]
+					pos = int(pos[4:])
+
+					snpClass = headerSplt[7]
+					snpClass = snpClass[6:]
+					stdSNP = (snpClass == '1')
+						
+					alleles = headerSplt[8]
+					alleles = alleles[8:]
+					alleles = alleles.replace('\"','')
+					alleles = alleles.split('/')
+					alleleNum = len(alleles)
+					
+				elif line[0]=="\n":
+					if unique and gene and stdSNP and (pos>25):
+						sequence = sequence.replace('\n', '')
+						sequence = sequence.replace(' ', '')
+						sequence += '\n'
+						
+						if len(sequence) < 50:
+							continue
+							
+						for allele in alleles:
+							seqChop = sequence[pos-26:pos-1]
+							if allele=='-':
+								seqChop += ''
+							else:
+								seqChop += allele
+							seqChop += sequence[pos:pos+25]
+							seqChop += "\n" 
+							
+							# Modify header with allele info
+							label = header.split(" ")
+							label = label[0]
+							label += "|" + str(alleleNum)
+							label += "|" + allele
+							
+							# Print each header/sequence to file 
+							print("{}".format(label), file=text_file)
+							print("{}".format(seqChop), file=text_file)
+							
+							# Increment counter
+							count += 1
+							if v:
+								if count%10000==0:
+									print(count)
+				elif line[0]=="#":
+					#Do nothing, it's a comment line
+					pass
+				else:
+					sequence += line
 		
 def rsSearch(rsNumber):
 	global index
